265dfa7ad9437930e2b9c4ce1c0beb0e6f81e03f
leeleesim <97943003+leeleesim@users.noreply.github.com>
2022-01-28 20:37:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 @app.route("/")
 def home_view():
 	return render_template('index.html')
+	return render_template('Contact.html')
+	return render_template('Home.css')
+	return render_template('jquery.js')
+	return render_template('nicepage.css')
+	return render_template('index.html')
+	return render_template('nicepage.js')
 	
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080)
